f557fd48eb87790fc6ca44cfb68da01bd8728ab4
JesusChristOurGod <goryachev.web@gmail.com>
2021-07-02 17:26:41 +0300

    Initial Commit

---
+++ HSE_Git/HSE_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSE_GitApp.swift
+//  HSE_Git
+//
+//  Created by Горячев Александр on 02.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct HSE_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
